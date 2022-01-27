020410c455f1ca56927a2e244b81304fb7923a62
Sebastian Frysztak <sebastian@frysztak.dev>
2022-01-27 11:54:48 +0100

    fix(fe/Login): fix layout, add color mode to login page

--- packages/frontend/pages/login/index.page.tsx
+++ packages/frontend/pages/login/index.page.tsx
@@ -1,5 +1,6 @@
 import React, { useCallback } from 'react';
-import { NextPage } from 'next';
+import type { NextPageWithLayout } from '@pages/types';
+import type { GetServerSideProps } from 'next';
 import { useRouter } from 'next/router';
 import Head from 'next/head';
 import { useMutation } from 'react-query';
@@ -7,7 +8,7 @@ import { Heading, VStack, Container } from '@chakra-ui/react';
 import { useApi, useHandleError } from '@api';
 import { LoginForm, LoginFormData } from './LoginForm';
 
-const LoginPage: NextPage = () => {
+const LoginPage: NextPageWithLayout = () => {
   const api = useApi();
   const router = useRouter();
   const { onError } = useHandleError();
@@ -38,6 +39,20 @@ const LoginPage: NextPage = () => {
       </Container>
     </>
   );
+};
+
+LoginPage.getLayout = (page) => {
+  return page;
+};
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  const cookies = req.headers.cookie ?? '';
+
+  return {
+    props: {
+      cookies,
+    },
+  };
 };
 
 export default LoginPage;
