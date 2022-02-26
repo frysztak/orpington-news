d3204ef3f8f3482f8e5512d2d5ec5bc419393d76
Sebastian Frysztak <sebastian@frysztak.dev>
2022-02-26 22:54:58 +0100

    feat(fe/settings): add 'go back' button to SettingsSidebar

--- packages/frontend/pages/settings/components/sidebar/SettingsSidebar.tsx
+++ packages/frontend/pages/settings/components/sidebar/SettingsSidebar.tsx
@@ -1,4 +1,6 @@
-import { BoxProps, Heading, VStack } from '@chakra-ui/react';
+import NextLink from 'next/link';
+import { BoxProps, Button, Heading, VStack } from '@chakra-ui/react';
+import { IoReturnUpBack } from '@react-icons/all-files/io5/IoReturnUpBack';
 import { SettingsCategory } from './SettingsCategory';
 import { SettingsLink } from './SettingsLink';
 
@@ -7,6 +9,12 @@ export type SettingsSidebarProps = BoxProps;
 export const SettingsSidebar: React.FC<SettingsSidebarProps> = (props) => {
   return (
     <VStack w="full" align="flex-start" spacing={3} {...props}>
+      <NextLink href="/" passHref>
+        <Button mx={4} pb={4} leftIcon={<IoReturnUpBack />} variant="link">
+          Go back
+        </Button>
+      </NextLink>
+
       <Heading px={4} fontSize="2xl">
         Settings
       </Heading>
--- packages/frontend/pages/settings/index.page.tsx
+++ packages/frontend/pages/settings/index.page.tsx
@@ -1,7 +1,4 @@
 import Head from 'next/head';
-import NextLink from 'next/link';
-import { IconButton, VStack } from '@chakra-ui/react';
-import { IoReturnUpBack } from '@react-icons/all-files/io5/IoReturnUpBack';
 import type { NextPageWithLayout } from '@pages/types';
 import { commonGetServerSideProps } from '@pages/ssrProps';
 import { SettingsSidebar } from './components/sidebar/SettingsSidebar';
@@ -13,18 +10,7 @@ const Page: NextPageWithLayout = () => {
         <title>Settings</title>
       </Head>
 
-      <VStack spacing={2}>
-        <NextLink href="/" passHref>
-          <IconButton
-            icon={<IoReturnUpBack />}
-            aria-label="Go back to homepage"
-            variant="ghost"
-            mr="auto"
-          />
-        </NextLink>
-
-        <SettingsSidebar py={4} />
-      </VStack>
+      <SettingsSidebar py={4} />
     </>
   );
 };
