2784f2bc293868d1d7f1d72f7fcdad1adfdca81e
Sebastian Frysztak <sebastian@frysztak.dev>
2022-04-30 11:05:56 +0200

    feat(SidebarFooter): add hover state

--- packages/frontend/components/sidebar/SidebarFooter.tsx
+++ packages/frontend/components/sidebar/SidebarFooter.tsx
@@ -1,4 +1,10 @@
-import { HStack, Icon, Link } from '@chakra-ui/react';
+import {
+  Center,
+  HStack,
+  Icon,
+  Link,
+  useColorModeValue,
+} from '@chakra-ui/react';
 import NextLink from 'next/link';
 import { RiSettingsLine } from '@react-icons/all-files/ri/RiSettingsLine';
 import type { Preferences, User } from '@orpington-news/shared';
@@ -13,6 +19,7 @@ export interface SidebarFooterProps {
 export const SidebarFooter: React.FC<SidebarFooterProps> = (props) => {
   const { user, preferences } = props;
   const fg = useIconFill();
+  const hoverBg = useColorModeValue('purple.10', 'gray.600');
 
   return (
     <HStack justify="space-between">
@@ -23,7 +30,15 @@ export const SidebarFooter: React.FC<SidebarFooterProps> = (props) => {
       />
 
       <NextLink href="/settings" passHref>
-        <Link aria-label="Open settings">
+        <Link
+          aria-label="Open settings"
+          as={Center}
+          p={2}
+          borderRadius="md"
+          _hover={{
+            backgroundColor: hoverBg,
+          }}
+        >
           <Icon as={RiSettingsLine} w={8} h={8} color={fg} />
         </Link>
       </NextLink>
