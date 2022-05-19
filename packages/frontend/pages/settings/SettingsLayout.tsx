a65600a3fd099bdba8e374a719bef73fced65867
Sebastian Frysztak <sebastian@frysztak.dev>
2022-05-19 08:05:47 +0200

    fix(SettingsLayout): migrate from Show component to display media queries
    this fixes SSR warning

--- packages/frontend/pages/settings/SettingsLayout.tsx
+++ packages/frontend/pages/settings/SettingsLayout.tsx
@@ -1,5 +1,5 @@
 import NextLink from 'next/link';
-import { HStack, Divider, Show, VStack, Button } from '@chakra-ui/react';
+import { HStack, Divider, VStack, Button } from '@chakra-ui/react';
 import { IoReturnUpBack } from '@react-icons/all-files/io5/IoReturnUpBack';
 import { SettingsSidebar } from './components/sidebar/SettingsSidebar';
 
@@ -7,30 +7,43 @@ export interface SettingsLayoutProps {}
 
 export const SettingsLayout: React.FC<SettingsLayoutProps> = ({ children }) => {
   return (
-    <HStack w="full" h="full" align="flex-start" flexGrow={1}>
-      <Show above="sm">
+    <>
+      <HStack
+        display={{ base: 'none', sm: 'flex' }}
+        w="full"
+        h="full"
+        align="flex-start"
+        flexGrow={1}
+      >
         <HStack w={60} h="100vh" align="flex-start" px={2} flexShrink={0}>
           <SettingsSidebar py={4} />
           <Divider orientation="vertical" h="full" />
         </HStack>
         {children}
-      </Show>
+      </HStack>
 
-      <Show below="sm">
-        <VStack spacing={2} pt={4} px={2} w="full" align="flex-start">
-          <NextLink href="/settings" passHref>
-            <Button
-              mx={4}
-              height={8}
-              leftIcon={<IoReturnUpBack />}
-              variant="link"
-            >
-              Go back
-            </Button>
-          </NextLink>
-          {children}
-        </VStack>
-      </Show>
-    </HStack>
+      <VStack
+        display={{ base: 'flex', sm: 'none' }}
+        w="full"
+        h="full"
+        align="flex-start"
+        flexGrow={1}
+        spacing={2}
+        pt={4}
+        px={2}
+      >
+        <NextLink href="/settings" passHref>
+          <Button
+            mx={4}
+            height={8}
+            leftIcon={<IoReturnUpBack />}
+            variant="link"
+          >
+            Go back
+          </Button>
+        </NextLink>
+        {children}
+      </VStack>
+    </>
   );
 };
