5c25ea08516e6cbdc0e797614a8acb3b66573e02
Sebastian Frysztak <sebastian@frysztak.dev>
2022-06-11 15:44:02 +0200

    fix(fe): make icon skeleton a square

--- packages/frontend/components/sidebar/CollectionsSkeleton.tsx
+++ packages/frontend/components/sidebar/CollectionsSkeleton.tsx
@@ -3,7 +3,7 @@ import { HStack, SkeletonCircle, SkeletonText, VStack } from '@chakra-ui/react';
 const SidebarItemSkeleton: React.FC<{ nested?: boolean }> = ({ nested }) => {
   return (
     <HStack pl={nested ? 10 : 6} pr={3} spacing={4} w="full" h={10}>
-      <SkeletonCircle size="6" />
+      <SkeletonCircle size="6" flexShrink={0} />
       <SkeletonText noOfLines={1} py={2} w="full" h={10} skeletonHeight={6} />
     </HStack>
   );
