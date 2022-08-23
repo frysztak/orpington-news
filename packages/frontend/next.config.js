38276416d7ba7ee0f6a8ab1b69b8c23ae3b1e212
Sebastian Frysztak <sebastian@frysztak.dev>
2022-08-23 16:25:48 +0200

    chore: bump deps

--- packages/frontend/next.config.js
+++ packages/frontend/next.config.js
@@ -1,29 +1,19 @@
-/** @type {import('next').NextConfig} */
-
-const withPlugins = require('next-compose-plugins');
 const bundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
-const PWA = require('next-pwa');
-const runtimeCaching = require('./pwa/runtimeCaching');
+const PWA = require('next-pwa')({
+  dest: 'public',
+  runtimeCaching: require('./pwa/runtimeCaching'),
+  disable: process.env.NODE_ENV !== 'production',
+  register: false,
+  skipWaiting: false,
+});
 
-module.exports = withPlugins(
-  [
-    [bundleAnalyzer],
-    [
-      PWA,
-      {
-        pwa: {
-          dest: 'public',
-          runtimeCaching,
-          disable: process.env.NODE_ENV !== 'production',
-          register: false,
-          skipWaiting: false,
-        },
-      },
-    ],
-  ],
-  {
+module.exports = () => {
+  const plugins = [bundleAnalyzer, PWA];
+
+  /** @type {import('next').NextConfig} */
+  const nextConfig = {
     reactStrictMode: true,
     pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js'],
     swcMinify: true,
@@ -35,5 +25,7 @@ module.exports = withPlugins(
     },
     productionBrowserSourceMaps: process.env.SOURCE_MAPS === 'true',
     output: 'standalone',
-  }
-);
+  };
+
+  return plugins.reduce((acc, plugin) => plugin(acc), { ...nextConfig });
+};
