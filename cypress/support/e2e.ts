63dd4b55a6b3caa974c4233868a912e610f580f2
Sebastian Frysztak <sebastian@frysztak.dev>
2022-08-27 18:44:45 +0200

    fix(CI): disable code coverage for now

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -15,7 +15,7 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands';
-import '@cypress/code-coverage/support';
+// import '@cypress/code-coverage/support';
 
 require('cypress-dark');
 
