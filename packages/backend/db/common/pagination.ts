e9487ea8d2b99ac1baa85ed17e740528f2dddad1
Sebastian Frysztak <sebastian@frysztak.dev>
2022-08-27 13:32:48 +0200

    chore(be): fix build

--- packages/backend/db/common/pagination.ts
+++ packages/backend/db/common/pagination.ts
@@ -1,13 +1,15 @@
 import { Static, Type } from '@sinclair/typebox';
 import { sql, TaggedTemplateLiteralInvocation } from 'slonik';
 
+type UserQueryResultRow = Record<string, any>;
+
 export const PaginationSchema = Type.Object({
   pageSize: Type.Optional(Type.Integer()),
   pageIndex: Type.Optional(Type.Integer()),
 });
 export type PaginationParams = Static<typeof PaginationSchema>;
 
-export const addPagination = <TResult>(
+export const addPagination = <TResult extends UserQueryResultRow>(
   paginationParams: PaginationParams,
   query: TaggedTemplateLiteralInvocation<TResult>
 ) => {
