c2ffd944a8af3b6aa8d118b958ab8f3b65f4ec46
Sebastian Frysztak <sebastian@frysztak.dev>
2022-09-26 12:50:18 +0200

    feat(e2e): run login and signup tests for two viewports

--- cypress/e2e/signup.cy.ts
+++ cypress/e2e/signup.cy.ts
@@ -1,20 +1,28 @@
 import { getApiPath } from './utils';
 
-describe('signup page', () => {
-  it('can successfully sign up', () => {
-    cy.intercept({
-      method: 'POST',
-      url: getApiPath('/auth/register'),
-    }).as('apiAuthRegister');
+const sizes = ['macbook-13', 'iphone-6'];
 
-    cy.visit('/signup');
-    cy.getBySel('username').type('end2end');
-    cy.getBySel('displayName').type('E2E');
-    cy.getBySel('password').type('end2endpass');
-    cy.getBySel('passwordConfirm').type('end2endpass');
-    cy.getBySel('submit').click();
+sizes.forEach((size) => {
+  describe(`signup page, size '${size}'`, () => {
+    beforeEach(() => {
+      cy.viewport(size as any);
+    });
 
-    cy.wait('@apiAuthRegister').its('response.statusCode').should('eq', 200);
-    cy.url().should('include', '/login');
+    it('can successfully sign up', () => {
+      cy.intercept({
+        method: 'POST',
+        url: getApiPath('/auth/register'),
+      }).as('apiAuthRegister');
+
+      cy.visit('/signup');
+      cy.getBySel('username').type('end2end');
+      cy.getBySel('displayName').type('E2E');
+      cy.getBySel('password').type('end2endpass');
+      cy.getBySel('passwordConfirm').type('end2endpass');
+      cy.getBySel('submit').click();
+
+      cy.wait('@apiAuthRegister').its('response.statusCode').should('eq', 200);
+      cy.url().should('include', '/login');
+    });
   });
 });
