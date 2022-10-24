a8e0501eb609c712bbb5729dbcbde780ae6a09e9
Sebastian Frysztak <sebastian@frysztak.dev>
2022-10-24 21:14:28 +0200

    feat(e2e): additional tests for signup and login flows

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -6,16 +6,49 @@ sizes.forEach((size) => {
   describe(`login page, size '${size}'`, () => {
     beforeEach(() => {
       cy.viewport(size as any);
-      cy.signupByApi('end2end', 'end2endpass', 'E2E');
     });
 
     const baseUrl = Cypress.config('baseUrl');
+
+    it("cannot log in if user doesn't exist", () => {
+      cy.intercept({
+        method: 'POST',
+        url: getApiPath('/auth/login'),
+      }).as('apiAuthLogin');
+
+      cy.visit('/login');
+      cy.getBySel('username').type('end2end');
+      cy.getBySel('password').type('end2endpass');
+      cy.getBySel('submit').click();
+
+      cy.wait('@apiAuthLogin').its('response.statusCode').should('eq', 403);
+      cy.url().should('equal', `${baseUrl}/login`);
+    });
+
+    it('cannot log in if password is incorrect', () => {
+      cy.intercept({
+        method: 'POST',
+        url: getApiPath('/auth/login'),
+      }).as('apiAuthLogin');
+
+      cy.signupByApi('end2end', 'end2endpass', 'E2E');
+
+      cy.visit('/login');
+      cy.getBySel('username').type('end2end');
+      cy.getBySel('password').type('wrongpass');
+      cy.getBySel('submit').click();
+
+      cy.wait('@apiAuthLogin').its('response.statusCode').should('eq', 403);
+      cy.url().should('equal', `${baseUrl}/login`);
+    });
 
     it('can successfully log in', () => {
       cy.intercept({
         method: 'POST',
         url: getApiPath('/auth/login'),
       }).as('apiAuthLogin');
+
+      cy.signupByApi('end2end', 'end2endpass', 'E2E');
 
       cy.visit('/login');
       cy.getBySel('username').type('end2end');
