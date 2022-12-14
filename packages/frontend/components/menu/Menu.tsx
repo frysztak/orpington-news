2c5eeb378832281cb5988a3b03a1c88b6041f17e
Sebastian Frysztak <sebastian@frysztak.dev>
2022-12-14 22:59:08 +0100

    feat(ArticleHeader): use Chakra-free Menu

--- packages/frontend/components/menu/Menu.tsx
+++ packages/frontend/components/menu/Menu.tsx
@@ -1,5 +1,6 @@
-import React, { PropsWithChildren, ReactNode } from 'react';
+import React, { forwardRef, PropsWithChildren, ReactNode } from 'react';
 import * as DropdownMenuPrimitive from '@radix-ui/react-dropdown-menu';
+import { CheckIcon } from '@radix-ui/react-icons';
 import cx from 'classnames';
 
 export const Menu = DropdownMenuPrimitive.Root;
@@ -19,7 +20,6 @@ export const MenuContent: React.FC<PropsWithChildren<{}>> = ({
         'border border-gray-200 dark:border-gray-600',
         'bg-white dark:bg-gray-700'
       )}
-      data-focus-visible-disabled
       {...rest}
     >
       {children}
@@ -42,7 +42,8 @@ export const MenuItem: React.FC<MenuItemProps> = ({
         'flex gap-x-2 cursor-default select-none items-center px-2 py-2 outline-none',
         'focus:bg-gray-50 dark:focus:bg-whiteAlpha-100',
         'text-gray-700 dark:text-gray-100',
-        'cursor-pointer'
+        'cursor-pointer',
+        'radix-disabled:opacity-40 radix-disabled:cursor-not-allowed'
       )}
       onClick={(event) => event.stopPropagation()}
       {...rest}
@@ -57,3 +58,48 @@ MenuItem.displayName = 'MenuItem';
 export const MenuDivider = () => (
   <DropdownMenuPrimitive.Separator className="my-1 h-px bg-gray-200 dark:bg-gray-600" />
 );
+
+export const MenuLabel = forwardRef<
+  HTMLDivElement,
+  DropdownMenuPrimitive.MenuLabelProps
+>(({ children, ...props }, forwardedRef) => {
+  return (
+    <DropdownMenuPrimitive.Label
+      {...props}
+      ref={forwardedRef}
+      className="px-4 py-2 font-semibold text-sm"
+    >
+      {children}
+    </DropdownMenuPrimitive.Label>
+  );
+});
+MenuLabel.displayName = 'MenuLabel';
+
+export const MenuRadioGroup = DropdownMenuPrimitive.RadioGroup;
+
+export const MenuRadioItem = forwardRef<
+  HTMLDivElement,
+  DropdownMenuPrimitive.DropdownMenuRadioItemProps
+>(({ children, ...props }, forwardedRef) => {
+  return (
+    <DropdownMenuPrimitive.RadioItem
+      {...props}
+      ref={forwardedRef}
+      className={cx(
+        'flex gap-x-2 cursor-default select-none items-center px-2 py-2 outline-none',
+        'focus:bg-gray-50 dark:focus:bg-whiteAlpha-100',
+        'text-gray-700 dark:text-gray-100',
+        'cursor-pointer'
+      )}
+    >
+      <DropdownMenuPrimitive.ItemIndicator
+        forceMount
+        className="radix-state-unchecked:opacity-0 radix-state-checked:opacity-1"
+      >
+        <CheckIcon />
+      </DropdownMenuPrimitive.ItemIndicator>
+      {children}
+    </DropdownMenuPrimitive.RadioItem>
+  );
+});
+MenuRadioItem.displayName = 'MenuRadioItem';
