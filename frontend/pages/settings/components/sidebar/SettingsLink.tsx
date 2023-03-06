f1f934369b72cfbc94a724617e447f9340ed3813
Sebastian Frysztak <sebastian@frysztak.dev>
2023-03-06 18:23:31 +0100

    refactor(fe): simplify SettingsLink

--- frontend/pages/settings/components/sidebar/SettingsLink.tsx
+++ frontend/pages/settings/components/sidebar/SettingsLink.tsx
@@ -4,49 +4,36 @@ import { useRouter } from 'next/router';
 import NextLink from 'next/link';
 import { BsChevronRight } from '@react-icons/all-files/bs/BsChevronRight';
 
-const StyledLink = React.forwardRef(function StyledLink(
-  props: PropsWithChildren<{ isActive?: boolean }>,
-  ref: React.Ref<any>
-) {
-  const { isActive, children, ...rest } = props;
-
-  return (
-    <a
-      aria-current={isActive ? 'page' : undefined}
-      className={cx(
-        'cursor-pointer',
-        'flex items-center justify-between',
-        'w-full px-4 py-1 rounded-md transition-all text-sm',
-        {
-          'text-gray-700 dark:text-white font-medium': !isActive,
-          'bg-purple-50 dark:bg-purple-100 text-purple-700 font-semibold':
-            isActive,
-        }
-      )}
-      ref={ref}
-      {...rest}
-    >
-      {children}
-      <BsChevronRight className="md:hidden" />
-    </a>
-  );
-});
-
 export type SettingsLinkProps = PropsWithChildren<{
   href: string;
   className?: string;
 }>;
 
 export const SettingsLink: React.FC<SettingsLinkProps> = (props) => {
-  const { href, children, ...rest } = props;
+  const { href, children, className } = props;
 
   const { asPath } = useRouter();
   const isActive = asPath === href;
 
   return (
-    <div className="select-none flex items-center leading-6" {...rest}>
-      <NextLink href={href} passHref legacyBehavior>
-        <StyledLink isActive={isActive}>{children}</StyledLink>
+    <div className="select-none flex items-center leading-6">
+      <NextLink
+        href={href}
+        aria-current={isActive ? 'page' : undefined}
+        className={cx(
+          'cursor-pointer',
+          'flex items-center justify-between',
+          'w-full px-4 py-1 rounded-md transition-all text-sm',
+          {
+            'text-gray-700 dark:text-white font-medium': !isActive,
+            'bg-purple-50 dark:bg-purple-100 text-purple-700 font-semibold':
+              isActive,
+          },
+          className
+        )}
+      >
+        {children}
+        <BsChevronRight className="md:hidden" />
       </NextLink>
     </div>
   );
